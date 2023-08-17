822579013cf074c8dae8e806ad1251ee7bffbb64
Raul Haro <raulharo99@gmail.com>
2023-08-16 23:59:50 -0400

    Add project files.

---
+++ Jokes Web App/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Jokes_Web_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Jokes_Web_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
